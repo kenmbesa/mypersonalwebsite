fb49c482461fed621b167ece406a7d887aeb6848
Ken Mbesa <kenmbesa@gmail.com>
2021-10-27 06:43:41 +0300

    Added improvements to the responsive navbar

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,17 @@ function toggleMobileMenu() {
     myMenu.style.display = "block";
   }
 }
+
+// function changeText () {
+//   const btn = document.getElementById('buttn');
+//   btn.innerHTML = "My Projects";
+
+//   btn.addEventListener('click', function () {
+//     if (btn.innerHTML = "My Projects") {
+//       btn.innerHTML = "Build a web app";
+//     } else if (btn.innerHTML = "Build a web app") {
+//       btn.innerHTML = "My Projects";
+//     }
+//   })
+
+// }
